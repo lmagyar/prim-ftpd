5d587be9dbbcffb4e276709c9a7aeeb66b47d91b
wolpi <wolpi.dev@web.de>
2024-06-08 11:06:15 +0200

    Moved logic that isExec==isDir up in class hierarchy. See GH issue #347

--- primitiveFTPd/src/org/primftpd/filesystem/SafSshFile.java
+++ primitiveFTPd/src/org/primftpd/filesystem/SafSshFile.java
@@ -69,12 +69,6 @@ public class SafSshFile extends SafFile<SshFile> implements SshFile {
     }
 
     @Override
-    public boolean isExecutable() {
-        logger.trace("[{}] isExecutable()", name);
-        return isDirectory;
-    }
-
-    @Override
     public boolean create() throws IOException {
         logger.trace("[{}] create()", name);
         return createNewFile();
